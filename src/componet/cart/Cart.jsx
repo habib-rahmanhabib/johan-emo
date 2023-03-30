9794e64ec56779dd098f526cb12695221f9f967e
habib-rahmanhabib <mdhabiburrahman80680@gmail.com>
2023-03-30 21:31:10 +0600

    complite all work

--- src/componet/cart/Cart.jsx
+++ src/componet/cart/Cart.jsx
@@ -4,18 +4,30 @@ import React from 'react';
 
 const Cart = ({cart}) => {
 
+    console.log(cart)
 let total= 0;
+let shipping =0;
+let quantity= 0;
+
     for(const product of cart){
-        total= total + product.price
+        if(product.quantity===0){
+            product.quantity=1
+        }
+        // product.quantity = product.quantity || 1
+        quantity = quantity + product.quantity
+        total= total + product.price * product.quantity
+        shipping= shipping + product.shipping
     }
+    const tax = shipping*7/100
+    const grandTotal = total + shipping + tax
     return (
         <div className='cart'>
-            <h3>Order Summary</h3>
-              <p>Selected product: {cart.length}</p>
+            <h4>Order Summary</h4>
+              <p>Selected product: {quantity}</p>
               <p>Total price:${total}</p>
-              <p>Total Shipping: $</p>
-              <p>Text: </p>
-              <p>Grand Total:$</p>
+              <p>Total Shipping: ${shipping}</p>
+              <p>Tax: {tax.toFixed(2)} </p>
+              <p>Grand Total:$ {grandTotal}</p>
 
         </div>
     );
--- src/componet/shop/Shop.jsx
+++ src/componet/shop/Shop.jsx
@@ -2,6 +2,8 @@ import './Shop.css'
 import React, { useEffect, useState } from 'react';
 import Product from '../../product/Product';
 import Cart from '../cart/Cart';
+import { addToDb, getShoppingCart} from '../../fakedb'
+
 
 const Shop = () => {
     const [products, setProducts]=useState([])
@@ -17,7 +19,24 @@ const Shop = () => {
     const eventHandler =(product)=>{
         const newCard =[...card, product]
         setCard(newCard)
-    }
+        addToDb(product.id)
+        
+    };
+    useEffect(()=> {
+        const storeCart = getShoppingCart()
+        const saveCard =[]
+        for(const id in storeCart){
+            const addedProduct = products.find(product=> product.id===id)
+           if(addedProduct){
+            const quantity = storeCart[id];
+            addedProduct.quantity =quantity;
+            saveCard.push(addedProduct)
+            
+           }
+        }
+        setCard(saveCard)
+      
+    },[products])
 
     return (
         <div className='shop-container'>
